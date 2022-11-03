aa308550003cff019fece9fd04a5a55b90e25d93
Abdelrahman <abdelrahman19.adel@gmail.com>
2022-11-03 14:24:38 +0200

    first commit

---
+++ hello.py
@@ -0,0 +1,16 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Flask Hello World!!'
+
+
+@app.route('/abdoadel')
+def test():
+    return 'Hello From Abdelrahman Adel'
+
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=5000)
